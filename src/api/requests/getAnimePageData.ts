69a895ba6d8c0d9304089bdbdb386e168c603ee4
olyalass <olyalass@icloud.com>
2024-05-09 14:21:39 +0200

    refactor: delete unnecessary try/catch

--- src/api/requests/getAnimePageData.ts
+++ src/api/requests/getAnimePageData.ts
@@ -1,17 +1,13 @@
 import { AnimeBaseResponseType } from '../../types'
 
 async function getAnimePageData(url: string) {
-  try {
-    const response = await fetch(url)
-    if (response.status === 404) {
-      return null
-    }
-    const data: { data: AnimeBaseResponseType } = await response.json()
-
-    return data.data
-  } catch {
-    throw new Error()
+  const response = await fetch(url)
+  if (response.status === 404) {
+    return null
   }
+  const data: { data: AnimeBaseResponseType } = await response.json()
+
+  return data.data
 }
 
 export default getAnimePageData
