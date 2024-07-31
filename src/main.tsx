cca50a092ddd7e60b6046c20512872d6976b7ceb
olyalass <olyalass@icloud.com>
2024-07-31 17:15:42 +0200

    revert: revert vite config changes

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,7 @@ const store: Store<RootState, ActionType> & { dispatch: DispatchType } =
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/movie-lib/dist">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
